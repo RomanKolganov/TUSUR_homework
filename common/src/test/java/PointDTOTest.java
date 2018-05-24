45a98cd2f0874c49297dc48b8561873302838e91
Roman Kolganov <ramoni1990@gmail.com>
2018-05-24 18:31:23 +0300

    Добавление decodeDTO для преобразования JSON строки в объект.

--- common/src/test/java/PointDTOTest.java
+++ common/src/test/java/PointDTOTest.java
@@ -1,8 +1,13 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PointDTOTest {
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\"}";
+    private String auroID = "o567gfd";
+
     @Test
     public void toJson() throws Exception {
         PointDTO point = new PointDTO();
@@ -11,5 +16,13 @@ public class PointDTOTest {
         point.setAutoId("o567gfd");
         assertTrue(point.toJson().contains("\"lat\":56"));
         System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDTO() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(expected, PointDTO.class);
+        assertEquals(auroID, dto.getAutoId());
+
     }
 }
